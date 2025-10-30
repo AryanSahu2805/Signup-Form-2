b8a032c69f4fcf1ee8905340e0932de004250b9d
Aryan Sahu <asahustudent1@gmail.com>
2025-10-30 18:20:18 -0400

    Updated welcome screen

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,7 +1,7 @@
 // lib/screens/welcome_screen.dart
 import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
-import 'signup_screen.dart'; // Import for navigation
+import 'signup_screen.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
